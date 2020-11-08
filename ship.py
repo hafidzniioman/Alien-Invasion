221258de679fc9b1532529184090fa5840dc07d1
hafidzniioman <hafidzniioman@gmail.com>
2020-11-08 21:19:39 +0700

    Respond to keypress both left and right

--- ship.py
+++ ship.py
@@ -8,14 +8,17 @@ class Ship:
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
-
+        # Movement Flags
         self.moving_right = False
+        self.moving_left = False
 
     
     def update(self):
+        """Update the ship's position based on movement flags."""
         if self.moving_right:
             self.rect.x += 1
-
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
