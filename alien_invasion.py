5070691b1bc5241cb007180328ac0ff962f4687e
hafidzniioman <hafidzniioman@gmail.com>
2020-11-23 23:09:23 +0700

    limiting the number of bullets and creating method

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,14 +19,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of bullets that have dissappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                print(len(self.bullets))
-                
+            self._update_bullets()
             self._update_screen()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -67,9 +60,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+            # Get rid of bullets that have dissappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
